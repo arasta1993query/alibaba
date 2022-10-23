2481417236672ac13527eafc0d232d2c2012b3d1
Alireza Tabrizi <alias1371@gmail.com>
2022-10-23 09:38:59 +0330

    make components and add style to input element

---
+++ src/components/CustomSelect.vue
@@ -0,0 +1,41 @@
+<script setup>
+import SuggestionBox from "@/components/SuggestionBox.vue";
+
+</script>
+
+<template>
+  <div class="custom-select">
+    <input type="text" class="input" placeholder="Add tags...">
+    <SuggestionBox />
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+  .custom-select{
+    position: relative;
+  }
+
+  .input{
+    width: 100%;
+    background: #ffffff;
+    border: 1px solid #F8F8F8;
+    padding: 8px;
+    font-size: 14px;
+    line-height: 19px;
+    border-radius: 8px;
+    outline: none;
+    font-family: 'Roboto', sans-serif;
+
+    &:focus {
+      box-shadow: 0px 4px 8px -2px rgba(27, 46, 94, 0.04);
+      border: 1px solid #F8F8F8;
+
+    }
+    &::placeholder{
+      color: #BABFC7;
+    }
+  }
+</style>
